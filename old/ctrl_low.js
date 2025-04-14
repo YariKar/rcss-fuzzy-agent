ddd5544b7e3c89d7d4aa1c53aa1ed4ab6c3cc8dc
YariKar <kyaroslav03@gmail.com>
2025-04-14 20:40:18 +0300

    priority flip

--- old/ctrl_low.js
+++
@@ -1,9 +0,0 @@
-const CTRL_LOW_GOALIE = {
-    execute(taken, controllers, fuzzy) {
-        const immediateAction = fuzzy.evaluateGoalie(taken.state);
-        console.log("GOALIE LOW", immediateAction)
-        return immediateAction || controllers[1].execute(taken, controllers, fuzzy);
-    }
-};
-
-module.exports = CTRL_LOW_GOALIE;
